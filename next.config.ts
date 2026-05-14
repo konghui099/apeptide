6105c6f42cf816aa700ad38e6c129a1f63452cb0
kongyahui <33146698+konghui099@users.noreply.github.com>
2026-05-14 14:25:56 +0800

    Update next.config.ts
    标点符号错误修改

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   output: 'export',
-  reactStrictMode: true，
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
